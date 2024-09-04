aad2cc87d72f8e67d0d2740503430221c2d4568b
bilalfarid-1 <bilaldrosh1@gmail.com>
2024-09-04 09:52:26 +0500

    Add new subtraction function to js file,

--- new.js
+++ new.js
@@ -5,3 +5,11 @@ function sum(a,b) {
 
 let num1 = 5, num2 = 3;
 sum(num1, num2);
+
+function sub(a,b) {
+    let sum = a - b;
+    console.log(sum);
+}
+
+let num_1 = 5, num_2 = 3;
+sub(num1, num2);
